dd3a66d151a6881c94b4ec48f21fcfd56961ed59
Feather <lisafeather@gmail.com>
2014-06-22 15:27:26 -0600

    inject response object through router into into request handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,20 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
   exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write(stdout);
+    response.end();
   });
-  return content;
 }
 
 function upload() {
   console.log("Request handler 'upload' was called.");
-  return "hello upload";
+  response.writeHead(200, { "Content-type": "text/plain"});
+  response.write("hello upload");
+  response.end();
 }
 
 exports.start = start;
