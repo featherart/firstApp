eae82dcbee35ce040dae88e977430ef2484d423f
Feather <lisafeather@gmail.com>
2014-06-22 15:30:57 -0600

    non-blocking long-running request

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,14 +3,15 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("ls -lah", function (error, stdout, stderr) {
+  exec("find /", { timeout: 10000, maxBuffer: 20000*1024 },
+       function (error, stdout, stderr) {
     response.writeHead(200, {"Content-type": "text/plain"});
     response.write(stdout);
     response.end();
   });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, { "Content-type": "text/plain"});
   response.write("hello upload");
