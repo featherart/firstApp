c90762ecf8bb45eed9ee099c775a80f4899456c8
Feather <lisafeather@gmail.com>
2014-06-23 11:54:14 -0600

    basic file upload working

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   console.log("about to route a request for: " + pathname);
   if( typeof handle[pathname] ==='function') {
-    handle[pathname](response, postData);
+    handle[pathname](response, request);
   } else {
     console.log("no request handler found for: " + pathname);
-    response.writeHead(404, {"Content-type": "text/plain"});
+    response.writeHead(404, {"Content-type": "text/html"});
     response.write("404 not found");
     response.end();
   }
--- server.js
+++ server.js
@@ -3,23 +3,11 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("request for " + pathname + " received.");
 
-    request.setEncoding("utf8");
-
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("received post data chunk '"+ postDataChunk +"'.");
-    });
-
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
-
+    route(handle, pathname, response, request);
   }
-
   http.createServer(onRequest).listen(8888);
   console.log("server has started");
 }
